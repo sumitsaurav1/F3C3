0bcbb8ce6b16d1df7a00039cefd10b04e8b3cc2b
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-05-28 00:33:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,25 +36,3 @@ function loadData(){
 
 }
 
-let profileDetails = document.getElementById("profile");
-window.addEventListener("DOMContentLoaded",()=>{
-    let userDetails = JSON.parse(localStorage.getItem('userDetails'));
-
-    if(userDetails){
-        profileDetails.innerHTML =`<h2>Profile</h2>
-        <div>Full Name : ${userDetails.name1}</div>
-        <div>Email : ${userDetails.email}</div>
-        <div>Password : ${userDetails.password}</div>`;
-    }
-    else{
-        window.location.href="index.html"
-    }
-})
-
-const logoutButton = document.getElementById('log-out');
-logoutButton.addEventListener('click', () => {
-  // Clear user state from local storage
-  localStorage.removeItem('userDetails');
-  // Redirect to signup page
-  window.location.href = 'index.html';
-});
